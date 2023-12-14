e471d74a69046fb4494fcf3105bddff03c6df57b
Berke Cengiz <berke.cengiz@kyndryl.com>
2023-12-14 17:50:22 +0300

    Edit profile updates

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -18,6 +18,7 @@ const ProfilePage = () => {
     medications: '',
     medicalConditions: ''
   });
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -35,6 +36,86 @@ const ProfilePage = () => {
     };
     fetchProfile();
   }, []);
+
+    const profileFields = [
+      { name: 'name', label: 'Name' },
+      { name: 'age', label: 'Age' },
+      { name: 'height', label: 'Height' },
+      { name: 'weight', label: 'Weight' },
+      { name: 'heartRate', label: 'Heart Rate' },
+      { name: 'vo2Max', label: 'VO2 Max' },
+      { name: 'bloodType', label: 'Blood Type' },
+      { name: 'allergies', label: 'Allergies' },
+      { name: 'medications', label: 'Medications' },
+      { name: 'medicalConditions', label: 'Medical Conditions' },
+    ];
+  
+  // Function to handle form submission
+  const handleUpdateProfile = async (event) => {
+    event.preventDefault();
+    try {
+      const token = localStorage.getItem('token');
+      await axios.put('/api/profile', profile, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setIsEditing(false); // Turn off editing mode after submission
+    } catch (error) {
+      console.error('Error updating profile:', error);
+    }
+  };
+  
+  if (isEditing) {
+    return (
+      <div className="flex flex-col min-h-screen bg-gray-100">
+      <main className="flex-grow">
+      <Header />
+      <div className="flex flex-col items-center justify-center mt-10">
+        <form onSubmit={handleUpdateProfile} className="w-full max-w-lg">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            {profileFields.map(({ name, label }) => (
+              <div key={name} className="w-full px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor={name}>
+                  {label}
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id={name}
+                  type="text"
+                  placeholder={`Enter ${label.toLowerCase()}`}
+                  name={name}
+                  value={profile[name]}
+                  onChange={(e) => setProfile({ ...profile, [name]: e.target.value })}
+                />
+              </div>
+            ))}
+          </div>
+          {/* Buttons */}
+          <div className="flex justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Save
+            </button>
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={() => setIsEditing(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+         </div>
+         </main>
+         <Footer />
+      </div>
+    );
+  }
+  
+  
 
   // Render the profile details
   return (
@@ -66,6 +147,13 @@ const ProfilePage = () => {
           </dl>
         </div>
       </div>
+      <button 
+      onClick={() => setIsEditing(true)}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto block mt-4"
+      >
+      Edit Profile
+    </button>
+            
     </div>
     <Footer />
     </div>
